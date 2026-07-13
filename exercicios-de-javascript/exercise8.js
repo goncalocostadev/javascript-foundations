2d11215ba703564bda37cf60b8d58030416b3ebd
goncalocostadev <goncalocostadev@users.noreply.github.com>
2026-07-14 00:55:36 +0100

    Add exercises for greeting, age conversion, salary calculation, month name retrieval, comparison, logical operations, range checking, and multiplication

---
+++ exercicios-de-javascript/exercise8.js
@@ -0,0 +1,14 @@
+function multiplicar(numero1, numero2) {
+    let resultado = 0;
+
+    
+    // vai adicionar o numero1 a quantidade de vezes que a quantidade do numero2 for, se numero 2 for "3" vai adicionar numero 1 ao resultado 3 vezes
+    // o for loop vai rodar o valor de numero2 e a cada volta vai adicionar o numero1 ao resultado, por isso se o numero2 for 4 e o numero1 for 5
+    // vai dar loop 4 vezes o numero 5 que vai ser 5+5+5+5
+    for (let i = 0; i < numero2; i++) {
+        resultado += numero1
+    }
+
+    return resultado
+}
+
