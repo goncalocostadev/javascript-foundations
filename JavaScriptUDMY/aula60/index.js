60a0326b77b16ac691401c917412759ad5c7f772
goncalocostadev <goncalocostadev@users.noreply.github.com>
2026-08-24 15:34:37 +0100

    Add array manipulation examples with push and splice methods

---
+++ JavaScriptUDMY/aula60/index.js
@@ -0,0 +1,11 @@
+// Valor por referencia
+// const nomes = ['Eduardo', 'Maria', 'Joana'];
+// String, Objetos, Funcoes, Numeros
+// const nomes = new Array('Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana');
+// const novo = nomes.slice(1, -2)
+
+
+const nomes = ['Eduardo', 'Maria', 'Joana'];
+const nome = nome.join(' ');
+console.log(nome)
+
---
+++ JavaScriptUDMY/aula61/index.js
@@ -0,0 +1,12 @@
+const nomes = ['Maria', 'Joao', 'Eduardo', 'Gabriel', 'Julia'];
+
+//nomes.splice(inidice, delete, elem1, elem2, elem3)
+//pop
+// const removidos = nomes.splice(-1, 1);
+
+//shift
+// const removidos = nomes.splice(0, 1);
+
+// Unshift
+nomes.push('Goncalo')
+console.log(nomes);
